04a80826256876803eb5aabc2cab8750c696e636
氷見悠人 <vzmc@hotmail.com>
2016-12-21 13:02:30 +0900

    2016.12.21 柏 particle追加

---
+++ TeamWorkGame/TeamWorkGame/TeamWorkGame/Utility/Particle.cs
@@ -0,0 +1,68 @@
+﻿///作成日：2016.12.21
+///作成者：柏
+///作成内容： パーティクル表現用クラス
+///最後修正内容：。。
+///最後修正者：。。
+///最後修正日：。。
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeamWorkGame.Device;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TeamWorkGame.Utility
+{
+    class Particle
+    {
+        private string name;
+        private Vector2 position;
+        private Vector2 startPosition;
+        private float size;
+        private int timer;
+        private bool isDead;
+        private Vector2 velocity;
+
+        public Particle(Vector2 velocity)
+        {
+            name = "firework";
+            position = new Vector2(210, 105);
+            startPosition = position;
+            isDead = false;
+            size = 5.0f;
+            timer = 0;
+            this.velocity = velocity;
+        }
+
+        public void Update()
+        {
+            timer++;
+            position += velocity;
+            if (timer % 2 == 0)
+            {
+                size += 0.1f;
+            }
+
+            float distence = (position.X - startPosition.X) * (position.X - startPosition.X) + (position.Y - startPosition.Y) * (position.Y - startPosition.Y);
+            if (distence >= 4000)
+            {
+                isDead = true;
+            }
+
+        }
+
+        public void Draw(Renderer renderer)
+        {
+            if (isDead) return;
+            renderer.DrawTexture(name, position, size);
+        }
+
+        public bool IsDead
+        {
+            get { return isDead; }
+        }
+
+    }
+}
---
+++ TeamWorkGame/TeamWorkGame/TeamWorkGame/Utility/ParticleControl.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeamWorkGame.Device;
+
+namespace TeamWorkGame.Utility
+{
+    class ParticleControl
+    {
+        private Timer addTimer;
+        private List<Particle> firworks;
+        private Timer fireTimer;
+        private bool fireSwitch;
+        private float[,] fireVelocity = new float[,]{
+            { 0,-1.5f}, { 0.5f,-1.2f }, { 1,-1 }, { 1.5f,-1.2f},
+            { 1.5f,0}, { 0.5f,1.2f }, { 1,1 }, { 1.5f,1.2f },
+            { 0,1.5f}, { -0.5f,1.2f }, { -1,1 }, { -1.5f,1.2f },
+            { -1.5f,0}, { -0.5f,-1.2f }, { -1,-1 }, { -1.5f,-1.2f },
+            };
+
+        public ParticleControl()
+        {
+            addTimer = new Timer(0.2f);
+            firworks = new List<Particle>();
+            fireTimer = new Timer(1.0f);
+            fireSwitch = false;
+        }
+
+
+        public void AddFireworks(Particle firework)
+        {
+            if (addTimer.IsTime())
+            {
+                firworks.Add(firework);
+                addTimer.Initialize();
+            }
+        }
+
+        public void Update()
+        {
+            addTimer.Update();
+            firworks.ForEach(f =>
+            {
+                f.Update();
+                if (f.IsDead) { fireSwitch = true; }
+            });
+            firworks.RemoveAll(f => f.IsDead);
+            if (fireSwitch) { Fire(); }
+        }
+
+
+        private void Fire()
+        {
+            fireTimer.Update();
+            for (int j = 0; j < fireVelocity.GetLength(0); j++)
+            {
+                AddFireworks(new Particle(3 * new Vector2(fireVelocity[j, 0], fireVelocity[j, 1])));
+            }
+
+            if (addTimer.IsTime())
+            {
+                addTimer.Initialize();
+            }
+
+            if (fireTimer.IsTime())
+            {
+                fireTimer.Initialize();
+                fireSwitch = false;
+            }
+        }
+
+
+        public void Draw(Renderer renderer)
+        {
+            firworks.ForEach(f => f.Draw(renderer));
+        }
+    }
+}
