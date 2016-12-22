8ef3bdfb1a966e1ae293b4a940f5d10f61097ee3
氷見悠人 <vzmc@hotmail.com>
2016-12-22 14:30:58 +0900

    炭取得時のエフェクト追加

--- TeamWorkGame/TeamWorkGame/TeamWorkGame/Actor/Coal.cs
+++ TeamWorkGame/TeamWorkGame/TeamWorkGame/Actor/Coal.cs
@@ -2,9 +2,9 @@
 //炭のクラス
 //作成時間：１０月１３日
 //By　佐瀬　拓海
-//最終更新日：１０月１９日
-//重力、あたり判定の追加
-//By　葉梨竜太
+//最終更新日：１２月２２日
+//炭取得時のエフェクト追加
+//By　佐瀬拓海
 ///////////////////////////////
 using Microsoft.Xna.Framework;
 using System;
@@ -24,6 +24,10 @@ namespace TeamWorkGame.Actor
         //追加　葉梨竜太
         private float gForce;
         private Map map;
+        //追加　佐瀬拓海
+        private bool isMove;
+        private bool prevIsMove;
+        private Vector2 coalPos;
 
         public Coal(Vector2 pos, Vector2 velo):
             base("coal", pos, velo, true, "coal")
@@ -34,6 +38,10 @@ namespace TeamWorkGame.Actor
         {
             base.Initialize();
             isToDeath = false;
+            //追加　佐瀬拓海
+            isMove = false;
+            prevIsMove = false;
+            coalPos = new Vector2(1024, 16);
 
             //追加　葉梨竜太
             gForce = Parameter.GForce;
@@ -50,31 +58,66 @@ namespace TeamWorkGame.Actor
        
         public override void Update(GameTime gameTime)
         {
-
-            //重力とあたり判定の追加
-            //葉梨竜太
-            velocity.Y += gForce;
-
-            //マップ上の物と障害物判定
-            foreach (var m in map.MapThings.FindAll(x => !x.IsTrigger))
+            if (isMove == false)//Playerとまだ触れてないとき
             {
-                ObstacleCheck(m);
+                //重力とあたり判定の追加
+                //葉梨竜太
+                velocity.Y += gForce;
+
+                //マップ上の物と障害物判定
+                foreach (var m in map.MapThings.FindAll(x => !x.IsTrigger))
+                {
+                    ObstacleCheck(m);
+                }
+
+                Method.MapObstacleCheck(ref position, localColRect, ref velocity, ref isOnGround, map, new int[] { 1, 2 });
+
+                //地面にいると運動停止
+                if (isOnGround)
+                {
+                    velocity = Vector2.Zero;
+                }
+
+                position += velocity;
+
+                //マップ上の物と衝突区域判定
+                foreach (var m in map.MapThings.FindAll(x => x.IsTrigger))
+                {
+                    CollisionCheck(m);
+                }
             }
-
-            Method.MapObstacleCheck(ref position, localColRect, ref velocity, ref isOnGround, map, new int[] { 1, 2 });
-
-            //地面にいると運動停止
-            if (isOnGround)
+            else//Playerと触れた後
             {
-                velocity = Vector2.Zero;
+                Move();
             }
-
-            position += velocity;
-
-            //マップ上の物と衝突区域判定
-            foreach (var m in map.MapThings.FindAll(x => x.IsTrigger))
+        }
+        public void Move()//画面右上のCoal表示まで移動させる
+        {
+            if (isMove)
             {
-                CollisionCheck(m);
+                if(position.X > coalPos.X)
+                {
+                    velocity.X = -10.0f;
+                }
+                if(position.X < coalPos.X)
+                {
+                    velocity.X = 10.0f;
+                }
+                if (position.Y > coalPos.Y)
+                {
+                    velocity.Y = -10.0f;
+                }
+                if (position.Y < coalPos.Y)
+                {
+                    velocity.Y = 10.0f;
+                }
+                position += velocity;
+
+                if(coalPos.Length() - position.Length() >= -5.0f && coalPos.Length() - position.Length() <= 5.0f)
+                {
+                    isDead = true;
+                }
+                prevIsMove = isMove;
             }
         }
         public override void EventHandle(GameObject other)
@@ -85,7 +128,23 @@ namespace TeamWorkGame.Actor
                 {
                     ((Player)other).FireNum = ((Player)other).FireNum + 1;
                 }
-                isDead = true;
+                prevIsMove = isMove;
+                isMove = true;
+            }
+        }
+        public override void Draw(GameTime gameTime, Renderer renderer, Vector2 offset, float cameraScale)//Playerと触れる前と触れる後の描画処理
+        {
+            if (!isMove)
+            {
+                base.Draw(gameTime, renderer, offset, cameraScale);
+            }
+            if (isMove)
+            {
+                if(prevIsMove == false && isMove == true)
+                {
+                    position += offset;
+                }
+                renderer.DrawTexture(name, position);
             }
         }
     }
